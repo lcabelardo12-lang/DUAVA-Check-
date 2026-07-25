4037a2e38ec7ab2029149696d22f91f711f0f7f3
lcabelardo12-lang <lcabelardo12@gmail.com>
2026-07-25 14:24:48 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+var C='duava-v4';
+self.addEventListener('install',function(e){
+  e.waitUntil(caches.open(C).then(function(c){return c.addAll(['index.html','app.js'])}));
+  self.skipWaiting();
+});
+self.addEventListener('activate',function(e){
+  e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.filter(function(k){return k!==C}).map(function(k){return caches.delete(k)}))}));
+  self.clients.claim();
+});
+self.addEventListener('fetch',function(e){
+  var u=e.request.url;
+  if(u.indexOf('script.google')!==-1)return;
+  e.respondWith(caches.match(e.request).then(function(r){return r||fetch(e.request)}));
+});
